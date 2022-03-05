cddeee9774ec27dc01de63b6d74ae871f889cd71
Gary Madsen <60731261+gajoma@users.noreply.github.com>
2022-03-05 11:03:10 +0100

    Add project files.

---
+++ Filtest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO; //Skal tilføjes!
+namespace Filtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            String path = @"tekstfil.txt"; //Filen skal placeres i: Source/Repos/Filtest/Filtest/bin/Debug/net5.0
+            String[] tekst = File.ReadAllLines(path); //Alle filens linjer indlæses i Array'et 'tekst'
+            for (int i = 0; i < tekst.Length; i++) 
+            {
+                Console.WriteLine(tekst[i]); //De enkelte linjer udskrives, en for en.
+            }
+            Console.ReadKey();
+            while (true) //Udskrivning af en tilfældig linje:
+            {
+                int linje = rnd.Next(0, tekst.Length);
+                Console.WriteLine();
+                Console.WriteLine(tekst[linje]);
+
+                Console.ReadKey();
+
+            }
+
+
+        }
+    }
+}
