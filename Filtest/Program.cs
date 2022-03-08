1e0f87350898172eb96129c4066ab2d88110edfb
Gary Madsen <60731261+gajoma@users.noreply.github.com>
2022-03-08 15:44:42 +0100

    Nu med rigtig mappeV3

--- Filtest/Program.cs
+++ Filtest/Program.cs
@@ -7,19 +7,21 @@ namespace Filtest
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            String path = @"tekstfil.txt"; //Filen skal placeres i: Source/Repos/Filtest/Filtest/bin/Debug/net5.0
+            String path = @"..\..\..\tekstfil.txt"; //Filen skal placeres i: Source/Repos/Filtest/Filtest/bin/Debug/net5.0
             String[] tekst = File.ReadAllLines(path); //Alle filens linjer indlæses i Array'et 'tekst'
             for (int i = 0; i < tekst.Length; i++) 
             {
                 Console.WriteLine(tekst[i]); //De enkelte linjer udskrives, en for en.
+                
             }
+            Console.WriteLine("\n Tast ENTER for at få en tilfældig linje fra filen\n");
             Console.ReadKey();
             while (true) //Udskrivning af en tilfældig linje:
             {
                 int linje = rnd.Next(0, tekst.Length);
                 Console.WriteLine();
                 Console.WriteLine(tekst[linje]);
-
+                
                 Console.ReadKey();
 
             }
